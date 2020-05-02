250838c24a792e990167e37917781f9e0a4728e0
Kyle Pace <kpace2296@gmail.com>
2020-05-01 20:53:44 -0500

    tested get method

---
+++ auth/auth-router.test.js
@@ -0,0 +1,9 @@
+const server = require('../api/server');
+const request = require('supertest');
+const router = require('./auth-router');
+
+// describe('server', () => {
+
+// })
+
+it.todo('run these tests')
